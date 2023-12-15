890a4c4dc6d858c15e2ac0b3222c1db24747657e
Matheus Augusto <auguzsto@gmail.com>
2023-12-15 19:55:38 -0300

    fix: config

--- backend/config.php
+++ backend/config.php
@@ -2,8 +2,7 @@
 
     global $config;
     
-    $config['host'] = "127.0.0.1";    // Change the host to 'mariadb' if you are using docker compose.
-    $config['port'] = "3306";       // Make sure there is no service on port 3306 if you use docker compose.
+    $config['host'] = "127.0.0.1";
+    $config['port'] = "3306";
     $config['user'] = "root";
     $config['password'] = "";
-    $config['database'] = "backend_assim";
