9f8153267bf1c9c4da240a5cbbbd22e2d9854811
Augusto <auguzsto@gmail.com>
2023-12-16 10:12:34 -0300

    fix: all relatorios + routes cors + api

--- backend/index.php
+++ backend/index.php
@@ -5,10 +5,6 @@ require "app/routes/routers.php";
 
 use App\core\Migration;
 
-    header('Access-Control-Allow-Origin: *');
-    header('Access-Control-Allow-Methods: GET, POST, OPTIONS, PATCH, PUT, DELETE');
-    header("Access-Control-Allow-Headers: X-Requested-With");
-
     error_reporting(0); // SET 0 FOR PROD, SET -1 FOR DEVELOPEMENT
     date_default_timezone_set("America/Sao_Paulo"); // DEFAULT CONFIG HOURS
     Migration::auto("15122023_1834.sql");
--- frontend/src/app/funcionarios/adicionar/page.tsx
+++ frontend/src/app/funcionarios/adicionar/page.tsx
@@ -2,17 +2,26 @@ import Header from "@/components/Header";
 import React from "react";
 import { addFuncionario, getAllCargos} from "../../../../api";
 import IFuncionario from "@/types/Funcionario";
-import InputText from "@/components/InputText";
-import InputTextNumber from "@/components/InputTextNumber";
-import { PatternFormat } from "react-number-format";
+import FormAddFuncionario from "../components/FormAddFuncionario";
 
 
 const AddFuncionario = async () => {
+    const funcionario = {
+        nome: '',
+        data_nascimento: '',
+        endereco_completo: '',
+        cpf: '',
+        email: '',
+        telefone: '',
+        cargo_id: 0
+    } as IFuncionario;
+
     const handleSubmitNewFuncionario = async (formData: FormData) => {
         'use server';
         const rawFormData = {
             nome: formData.get('nome'),
             data_nascimento: formData.get('data_nascimento'),
+            endereco_completo: formData.get('endereco_completo'),
             cpf: formData.get('cpf'),
             email: formData.get('email'),
             telefone: formData.get('telefone'),
@@ -28,46 +37,11 @@ const AddFuncionario = async () => {
         <div>
             <Header title="Funcionarios"/>
             <div className="p-1 text-2xl"><b>Adicionar funcionário</b></div>
-            <form action={handleSubmitNewFuncionario}>
-                <input
-                    required
-                    type="text" 
-                    name="nome"
-                    placeholder="Nome do funcionário" 
-                    className="input input-bordered w-full mt-2"
-                />
-                <input 
-                    type="date" 
-                    name="data_nascimento"
-                    placeholder="Data de nascimento" 
-                    className="input input-bordered w-full mt-2"/>
-                <InputTextNumber
-                    type="cpf"
-                    name="cpf"
-                    placeholder="CPF"
-                />
-                <input 
-                    type="text" 
-                    name="email"
-                    placeholder="Email" 
-                    className="input input-bordered w-full mt-2"/>
-                 <InputTextNumber
-                    type="telefone"
-                    name="telefone"
-                    placeholder="Telefone"
-                />
-                <select 
-                    required
-                    name="cargo_id"
-                    className="select select-bordered w-full mt-2">
-                    {cargos.map((cargo) => (
-                        <option key={cargo.id} value={cargo.id}>{cargo.nome}</option>
-                    ))}
-                </select>
-                <button
-                    className="btn btn-neutral w-full mt-2"
-                    type="submit">Adicionar</button>
-            </form>
+            <FormAddFuncionario 
+                cargos={cargos}
+                funcionario={funcionario}
+                handler={handleSubmitNewFuncionario}
+            />
         </div>
     );
 }
