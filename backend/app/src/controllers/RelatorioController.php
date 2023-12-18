20f781a5bb76b108061da67b7af258569b2a3f35
Matheus Augusto <auguzsto@gmail.com>
2023-12-18 20:55:44 -0300

    fix: reports return cargos and funcionarios deleted

--- backend/app/src/controllers/RelatorioController.php
+++ backend/app/src/controllers/RelatorioController.php
@@ -9,16 +9,19 @@ use App\core\Database;
         public function getNomeTelefoneCargoAll() {
             try {
                 $db = Database::getInstace();
-                $data = $db->query("SELECT 
-                f.id as funcionario_id, 
-                c.id as cargo_id, 
-                f.nome AS nome_funcionario, 
-                f.telefone, 
-                c.nome AS nome_cargo, 
-                c.salario FROM
-                funcionarios f
-                JOIN cargos c 
-                ON f.cargo_id = c.id")->fetchAll(PDO::FETCH_ASSOC);
+                $data = $db->query("SELECT
+                f.id as funcionario_id,
+                c.id as cargo_id,
+                f.nome as nome_funcionario,
+                f.telefone,
+                c.nome as nome_cargo,
+                c.salario as salario,
+                f.deleted_at,
+                c.deleted_at
+                FROM funcionarios f
+                JOIN cargos c ON f.cargo_id  = c.id 
+                AND f.deleted_at IS NULL 
+                AND  c.deleted_at  IS NULL")->fetchAll(PDO::FETCH_ASSOC);
                 echo json_encode($data);
             } catch (Exception $e) {
                 header('HTTP/1.1 400 Bad Request');
@@ -31,17 +34,19 @@ use App\core\Database;
             try {
                 $db = Database::getInstace();
                 $data = $db->query("SELECT
-                f.id AS funcionario_id,
-                c.id AS cargo_id,
-                f.nome AS nome_funcionario,
+                f.id as funcionario_id,
+                c.id as cargo_id,
+                f.nome as nome_funcionario,
                 f.telefone,
-                c.nome AS nome_cargo,
-                c.salario
-                FROM
-                funcionarios f
-                JOIN cargos c ON f.cargo_id = c.id
-                WHERE
-                c.nome LIKE '%$params%' OR f.nome LIKE '%$params%'")->fetchAll(PDO::FETCH_ASSOC);
+                c.nome as nome_cargo,
+                c.salario as salario,
+                f.deleted_at,
+                c.deleted_at
+                FROM funcionarios f
+                JOIN cargos c ON f.cargo_id  = c.id 
+                and f.deleted_at is null 
+                and c.deleted_at  is null
+                where c.nome LIKE '%$params%' OR f.nome LIKE '%$params%'")->fetchAll(PDO::FETCH_ASSOC);
                 echo json_encode($data);
             } catch (Exception $e) {
                 header('HTTP/1.1 400 Bad Request');
