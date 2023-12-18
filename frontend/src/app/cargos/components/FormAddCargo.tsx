1892186f58425216f819f378b1ec9f5fa95acbe7
Augusto <auguzsto@gmail.com>
2023-12-18 16:15:41 -0300

    fix: cargos handler add

--- frontend/src/app/cargos/components/FormAddCargo.tsx
+++ frontend/src/app/cargos/components/FormAddCargo.tsx
@@ -1,9 +1,9 @@
 'use client'
 
-import InputText from "@/components/InputText";
 import InputTextNumber from "@/components/InputTextNumber";
 import Modal from "@/components/Modal";
 import ICargo from "@/types/Cargo";
+import { useRouter } from "next/navigation";
 import React, { useRef, useState } from "react";
 
 interface IFormAddCargo {
@@ -13,33 +13,68 @@ interface IFormAddCargo {
 
 const FormAddCargo = (props: IFormAddCargo) => {
     const [modalOpen, setModalOpen] = useState(false)
+    const [valueNome, setValueNome] = useState("");
+    const [valueSalario, setValueSalario] = useState("");
+    const [isDisable, setIsDisable] = useState(true)
 
-    
-    
+    const router = useRouter();
+
+    const closeModal = () => {
+        setModalOpen(false);
+        router.push('/cargos');
+    }
+
+    const handlersNome = (value: string) => {
+        setValueNome(value);
+
+        if(value.length > 1) {
+            setValueSalario("0");
+            return setIsDisable(false);
+        }
+
+        return setIsDisable(true);
+    }
+
+    const handlersSalario = (value: string) => {
+        setValueSalario(value);
+
+        if(value.length > 1) {
+            return setIsDisable(false);
+        }
+
+        return setIsDisable(true);
+    }
+
     return (
         <>
-            <form action={props.handler} onChange={(e) => e}>
-                <InputText
+            <form action={props.handler}>
+                <input
+                    onChange={(e) => handlersNome(e.target.value)}
                     name="nome"
-                    value={props.cargo.nome}
+                    value={valueNome}
                     placeholder="Nome do cargo"
+                    className="input input-bordered w-full"
                 />
                 <InputTextNumber
+                    onChange={(e) => handlersSalario(e.target.value)}
                     id="salario"
-                    value={props.cargo.salario}
+                    value={valueSalario}
                     type="salario"
                     name="salario"
                     placeholder="Salário"
+                    className="input input-bordered w-full"
                 />
                 <div className="mt-2">
                     <button
+                        onClick={(e) => setModalOpen(true)}
+                        disabled={isDisable}
                         className="btn btn-neutral w-full"
                         type="submit">Adicionar</button>
                 </div>
             </form>
 
-            <Modal title="Atualização realizada" open={modalOpen} close={() => setModalOpen(false)}> 
-                Atualização nos atributos do cargo foram atualizadas com sucesso.
+            <Modal title="Adicionado" open={modalOpen} close={() => closeModal()}> 
+                Cargo adicionado com sucesso.
             </Modal>
             
         </>
